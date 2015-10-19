ba089609ab9ecdd2ddbff588d6a2c0bfba537393
meriy100 <ttattataa@gmail.com>
2015-10-19 14:56:34 +0900

    spec

---
+++ spec/dummy/app/controllers/post_controller.rb
@@ -0,0 +1,8 @@
+# spec/dummy/app/controller/post_controller.rb
+
+class PostController < ApplicationController
+  def index
+     @auto_i18n = AutoI18n::ViewHelpers::ActionView::Greet.new 'Bob'
+     @message = @auto_i18n.message
+  end
+end
