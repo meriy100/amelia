a9ec69f8666d7886a481d18a48f9a02c052fa53d
meriy100 <ttattataa@gmail.com>
2015-10-19 19:17:42 +0900

    10/19 last commit

--- spec/dummy/spec/views/post_spec.rb
+++ spec/dummy/spec/views/post_spec.rb
@@ -32,3 +32,33 @@ describe 'post/index.html.erb' do
   end
 end
 
+describe 'japan' do
+  before do
+    render
+  end
+
+  it "こんにちは" do
+    expect(rendered).to match "hello: こんにちは"
+  end
+  it "translate_false で hello" do
+    expect(rendered).to match "translate_false: hello"
+  end
+  it "html_safe では 自動置き換えしない" do
+    expect(rendered).to match "html_safe: hello"
+  end
+  it "count チェック 1 の時" do
+    expect(rendered).to match "Apple counter1: an apple"
+  end
+  it "count チェック 2 の時" do
+    expect(rendered).to match "Apple counter2: 2 apples"
+  end
+  it "DateTime: format long" do
+    expect(rendered).to match "DateTime: January 01, 2015 01:01"
+  end
+  it "Date:" do
+    expect(rendered).to match "Date: 2012-05-15"
+  end
+  it "Time: format short" do
+    expect(rendered).to match "Time: 31 Dec 23:59"
+  end
+end
