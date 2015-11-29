fcda0486a74b005ddab03afe196a8a68988281dd
meriy100 <ttattataa@gmail.com>
2015-11-29 18:00:09 +0900

    white space remove

--- lib/auto_i18n/action_view.rb
+++ lib/auto_i18n/action_view.rb
@@ -4,7 +4,7 @@ require 'action_view/helpers'
 
 module ActionView
   extend ActiveSupport::Autoload
-  
+
 # autoload :Helpers, "action_view/helpers"
 
   require 'active_support/core_ext/string/output_safety'
